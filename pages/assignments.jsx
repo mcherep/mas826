76be485fcae18dc0e9d3407e4ff5a9a87fea73ea
Manuel Cherep <manuelcherep@gmail.com>
2023-02-08 09:43:00 -0500

    Assignment 1

--- pages/assignments.jsx
+++ pages/assignments.jsx
@@ -1,4 +1,4 @@
-import { Title, Group, Text, Divider } from "@mantine/core";
+import { Title, Group, Text, Divider, Anchor } from "@mantine/core";
 
 const Assignments = () => {
     return (
@@ -12,14 +12,34 @@ const Assignments = () => {
                     Assignment 1
                 </Title>
                 <Title order={2} size={15} pt={"xl"} pb={0} color={"dimmed"}>
-                    (for February 15th)
+                    (due by/in class on February 15)
                 </Title>
             </Group>
             <Title order={2} size={14} p={"md"} pt={0} color={"dimmed"} style={{fontVariant: "small-caps"}}>
                 CREATIVITY: BIG-C AND LITTLE-C
             </Title>
-            <Text p={"md"} pt={0} pb={"xl"}>
-                Description
+            <Text td="underline" p={"md"} pt={0} pb={"xs"}>
+                Read
+            </Text>
+            <Text p={"xl"} pt={0} pb={"xl"}>
+                Amabile and Pillemer, <Anchor href="https://web.media.mit.edu/~tod/media/pdfs/Amabile_Perspectives-on-the-Social-Psychology-of-Creativity.pdf" target="blank_">Perspectives on the Social Psychology of Creativity</Anchor>
+                <br/>
+                Kaufman and Beghetto, <Anchor href="https://www.scinapse.io/papers/2131018663" target="blank_">Beyond Big and Little: The Four C Model of Creativity</Anchor>
+                <br/>
+                Neff, <Anchor href="https://www.tandfonline.com/doi/epdf/10.1080/07494467.2014.998414?needAccess=true&role=button" target="blank_">Point/Counterpoint: John Cage Studies with Arnold Schoenberg</Anchor>
+                <br/>
+                Oliveros, <Anchor href="https://www.whitefungus.com/pauline-oliveros-freedom-through-music" target="blank_">Freedom Through Music</Anchor>
+                <br/>
+                Minsky, <Anchor href="https://web.media.mit.edu/~minsky/papers/ComputersCantThink.txt" target="blank_">Why People Think Computers Can’t</Anchor>
+            </Text>
+
+            <Text td="underline" p={"md"} pt={0} pb={"xs"}>
+                Create
+            </Text>
+            <Text p={"xl"} pt={0} pb={"xl"}>
+                For next class (due on Feb. 15), create a 1-minute sound portrait to introduce yourself,
+                <br/>
+		highlighting/communicating some special aspect of your creative practice, style and/or experience.
             </Text>
 
             <Divider/>
@@ -29,14 +49,14 @@ const Assignments = () => {
                     Assignment 2
                 </Title>
                 <Title order={2} size={15} pt={"xl"} pb={0} color={"dimmed"}>
-                    (for February 22nd)
+                    (due by/in class on February 22)
                 </Title>
             </Group>
             <Title order={2} size={14} p={"md"} pt={0} color={"dimmed"} style={{fontVariant: "small-caps"}}>
                 MUSIC AS A TESTBED FOR CREATIVITY
             </Title>
             <Text p={"md"} pt={0} pb={"xl"}>
-                Description
+                TBD
             </Text>
         </>
     );
