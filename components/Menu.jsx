c0a53a724b651505779d781c547b63be0b758b61
Manuel Cherep <manuelcherep@gmail.com>
2023-02-07 22:31:32 -0500

    Trying to fix links

--- components/Menu.jsx
+++ components/Menu.jsx
@@ -5,12 +5,12 @@ const Menu = () => {
     return (
         <>
             <Stack p={"md"} pt={"md"} pb={0}>
-                <Anchor href="/" style={{ textDecoration: "none" }}><Title>Cultivating Creativity</Title></Anchor>
+                <Anchor href="" style={{ textDecoration: "none" }}><Title>Cultivating Creativity</Title></Anchor>
                 <br/>
-                <Anchor href="/syllabus" style={{ textDecoration: "none" }}>Syllabus</Anchor>
-                <Anchor href="/assignments" style={{ textDecoration: "none" }}>Assignments</Anchor>
-                <Anchor href="/projects" style={{ textDecoration: "none" }}>Projects</Anchor>
-                <Anchor href="/staff" style={{ textDecoration: "none" }}>Staff</Anchor>
+                <Anchor href="syllabus" style={{ textDecoration: "none" }}>Syllabus</Anchor>
+                <Anchor href="assignments" style={{ textDecoration: "none" }}>Assignments</Anchor>
+                <Anchor href="projects" style={{ textDecoration: "none" }}>Projects</Anchor>
+                <Anchor href="staff" style={{ textDecoration: "none" }}>Staff</Anchor>
             </Stack>
         </>
     );
