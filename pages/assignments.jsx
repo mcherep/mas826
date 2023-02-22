f1ffc863f8c0c1edd4f162162aa8f597aaa9e2fe
Manuel Cherep <manuelcherep@gmail.com>
2023-02-22 15:05:47 -0500

    Assignment 3

--- pages/assignments.jsx
+++ pages/assignments.jsx
@@ -81,6 +81,62 @@ const Assignments = () => {
                     Slides
                 </Button>
             </Group>
+
+            <Divider/>
+
+            <Group>
+                <Title order={2} size={25} p={"md"} pt={"xl"} pr={0} pb={0}>
+                    Assignment 3
+                </Title>
+                <Title order={2} size={15} pt={"xl"} pb={0} color={"dimmed"}>
+                    (due by class on March 1)
+                </Title>
+            </Group>
+            <Title order={2} size={14} p={"md"} pt={0} color={"dimmed"} style={{fontVariant: "small-caps"}}>
+                AI TOOLS FOR CREATIVITY
+            </Title>
+            <Text td="underline" p={"md"} pt={0} pb={"xs"}>
+                Read and Listen
+            </Text>
+            <Text p={"xl"} pt={0} pb={"xl"}>
+                George Lewis - Historic Perspective: <Anchor href="https://www-oxfordhandbooks-com.libproxy.mit.edu/view/10.1093/oxfordhb/9780190226992.001.0001/oxfordhb-9780190226992-e-29" target="blank_">Why Do We Want Our Computers to Improvise?</Anchor>
+                <br/>
+                <Anchor href="https://linkprotect.cudasvc.com/url?a=https%3a%2f%2fStability.ai&c=E,1,twsXS1d3YAyhkuhppO-HToWi6CmGt-rQGtFQz6piETqp-jAZ6hFXJH6hMkQEJduDjJW8iG2x6ntO8xVfBGJQqS3CUx6bIzyltr3_XCUYYo1Lav6yNd13_VEaBg,,&typo=1&ancr_add=1" target="blank_">Stability.ai</Anchor> - Modern Perspective: <Anchor href="https://linkprotect.cudasvc.com/url?a=https%3a%2f%2finterdependence.fm%2fepisodes%2fopen-source-ai-and-stable-diffusion-with-emad-mostaque&c=E,1,HXflt1BDtY_CL6w5t70HzVQopv5rPXlG60gSgFttCn__3UHTZgE7JXWyQi5BonZGJPr1cF5toyE3WYzHx0Y2V4PB82pMf-yb-FUOtfWuHYB0We__5sf7rHs,&typo=1" target="blank_">Holly Herndon’s podcast interview of Emad Mostaque</Anchor>
+                <br/>
+                Roger Dannenberg: <Anchor href="https://www.jstor.org/stable/pdf/3680940.pdf" target="blank_">Music Representation Issues, Techniques, and Systems</Anchor>
+                <br/>
+                Rebecca Fiebrink and Baptiste Caramiaux: <Anchor href="https://arxiv.org/pdf/1611.00379.pdf" target="blank_">The Machine Learning Algorithm as Creative Musical Tool</Anchor>
+                <br/>
+            </Text>
+            <Text td="underline" p={"md"} pt={0} pb={"xs"}>
+                Tools and Demos
+            </Text>
+            <Text p={"xl"} pt={0} pb={"xl"}>
+                Commercial tools for music making with very few parameters (genre, tempo): <Anchor href="https://www.aiva.ai/" target="blank_">AIVA</Anchor>, <Anchor href="https://www.ampermusic.com/" target="blank_">Amper</Anchor>, <Anchor href="https://www.beatoven.ai/" target="blank_">beatoven</Anchor>, <Anchor href="https://mubert.com/render/pricing?via=adk334" target="blank_">Mubert</Anchor>
+                <br/>
+                Foundation Models (<Anchor href="https://en.wikipedia.org/wiki/Foundation_models" target="blank_">ref</Anchor>) for AI generated Music with prompting: <Anchor href="https://openai.com/blog/jukebox/" target="blank_">Jukebox</Anchor>, <Anchor href="https://google-research.github.io/seanet/musiclm/examples/" target="blank_">MusicLM</Anchor>
+                <br/>
+                Living Singing AI (our work) [<Anchor href="https://web.media.mit.edu/~manaswim/Thesis_Media/Thesis/manaswi-MAS-2021-Thesis.pdf" target="blank_">paper</Anchor>] [<Anchor href="https://web.media.mit.edu/~manaswim/Thesis_Media/Demos/organismGallery/" target="blank_">demo</Anchor>] [<Anchor href="https://web.media.mit.edu/~manaswim/Thesis_Media/Demos/video/final_video.mp4" target="blank_">video</Anchor>], AI Radio (our work) [<Anchor href="https://arxiv.org/abs/2206.00485" target="blank_">paper</Anchor>] [<Anchor href="http://www.artificial.fm/" target="blank_">demo</Anchor>], DDSP [<Anchor href="https://sites.research.google/tonetransfer" target="blank_">demo</Anchor>], Rave [<Anchor href="https://github.com/acids-ircam/RAVE" target="blank_">github</Anchor>], Riffusion [<Anchor href="https://www.riffusion.com/" target="blank_">demo</Anchor>]
+            </Text>
+
+            <Text td="underline" p={"md"} pt={0} pb={"xs"}>
+                Extra
+            </Text>
+            <Text p={"xl"} pt={0} pb={"xl"}>
+                <Anchor href="https://www.youtube.com/watch?v=ec88U5R7cJ0" target="blank_">George Lewis (Voyager, 2011 performance ‘Interactive Trio’)</Anchor> - AI collaborator for performances
+                <br/>
+
+                Holly Herndon Ted Talk - <Anchor href="https://www.youtube.com/watch?v=5cbCYwgQkTE&ab_channel=TED" target="blank_">What if You Could Sing in Your Favorite Musician&apos;s Voice?</Anchor>
+                <br/>
+
+                <Anchor href="https://interdependence.fm/" target="blank_">Interdependence Podcast</Anchor> (Matt Dryhurst + Holly Herndon with prominent 21st century art+tech instigators)
+                <br/>
+
+                <Anchor href="https://linkprotect.cudasvc.com/url?a=https%3a%2f%2fualresearchonline.arts.ac.uk%2fid%2feprint%2f16687%2f1%2fFiebrinkTruemanCook_NIME2009.pdf&c=E,1,VUaVykuGnYz0wWL7aw9WWXTVOM58zlqRA-FZIbSYI6kh0a-8VspR456d43z9kPokJPU7WOPxuRwChTA5EJ0HDs1NwBDVh30imMs3uVSfk_tIvQ,,&typo=1" target="blank_">A Meta-Instrument for Interactive, On-the-fly Machine Learning</Anchor>
+                <br/>
+
+                <Anchor href="https://dl.acm.org/doi/pdf/10.1145/3414472" target="blank_">Designing Deep Reinforcement Learning for Human Parameter Exploration</Anchor>
+            </Text>
         </>
     );
 }
